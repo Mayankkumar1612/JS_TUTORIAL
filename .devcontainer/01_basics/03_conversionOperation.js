358f75d646f6daae46a7f037e1add4d9423a6522
MAYANK KUMAR <111239983+Mayankkumar1612@users.noreply.github.com>
2024-06-19 13:13:44 +0000

    operation studt

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = "Mayank"
 
 console.log(typeof score);
@@ -26,3 +27,33 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+// ****************************************OPERATIONS*************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Mayank"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
